44401c25ff4ea8955ac7daec2a598561c3afd70f
Laurens Eiroa <laurens.eiroasattler@protonmail.com>
2025-01-21 22:54:34 +0100

    .

--- src/coms/udp/udp_reciever.py
+++ src/coms/udp/udp_reciever.py
@@ -13,7 +13,7 @@ class UDPReceiver:
     async def receive_data(self, data_type="image"):
         print("receiving data")
         buffer = b''
-        max_dgram = Config.MAX_DGRAM_FRAME if data_type == "frame" else Config.MAX_DGRAM_DATA
+        max_dgram = Config.MAX_DGRAM_FRAME #if data_type == "frame" else Config.MAX_DGRAM_DATA
         while True:
             packet, _ = self.server_socket.recvfrom(max_dgram)
             if packet == b'END':
@@ -24,12 +24,12 @@ class UDPReceiver:
 async def run():
     receiver = Config.RECIEVER
 
-    #udp_data_receiver = UDPReceiver(receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT)
-    udp_frame_receiver = UDPReceiver(receiver_ip=Config.IPs[receiver], port=Config.UDP_FRAME_PORT)
+    udp_data_receiver = UDPReceiver(receiver_ip=Config.IPs[receiver], port=Config.UDP_DATA_PORT)
+    #udp_frame_receiver = UDPReceiver(receiver_ip=Config.IPs[receiver], port=Config.UDP_FRAME_PORT)
 
     while True:
         try:
-            data = None #await udp_data_receiver.receive_data(data_type="data")
+            data = await udp_data_receiver.receive_data(data_type="data")
         except Exception as e:
             print(e)
             data=None
@@ -37,26 +37,26 @@ async def run():
             data = data.decode('utf-8')
             print(f"received data: {data}")
         
-        try:
-            frame_data = await udp_frame_receiver.receive_data(data_type="frame")
-        except Exception as e:
-            print(e)
-            frame_data = None
-        if frame_data:
-            frame = cv2.imdecode(np.frombuffer(frame_data, dtype=np.uint8), cv2.IMREAD_COLOR)
-        
-            if data:
-                # Overlay the received data onto the frame
-                cv2.putText(frame, data, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        #try:
+        #    frame_data = await udp_frame_receiver.receive_data(data_type="frame")
+        #except Exception as e:
+        #    print(e)
+        #    frame_data = None
+        #if frame_data:
+        #    frame = cv2.imdecode(np.frombuffer(frame_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+        #
+        #    if data:
+        #        # Overlay the received data onto the frame
+        #        cv2.putText(frame, data, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-            # Display the frame with the overlay
-            cv2.imshow('Received Frame', frame)
+         #   # Display the frame with the overlay
+        #    cv2.imshow('Received Frame', frame)
 
         # Break the loop on 'q' key press
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #    break
 
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     asyncio.run(run())
